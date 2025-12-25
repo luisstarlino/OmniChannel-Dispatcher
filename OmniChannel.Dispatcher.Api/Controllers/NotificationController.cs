29a465cc5946cfb0e4cc840b0eb9f942a952d6b5
Luis Starlino <luis.guilherme009@gmail.com>
2025-12-25 20:13:31 -0300

    Add project files.

---
+++ OmniChannel.Dispatcher.Api/Controllers/NotificationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OmniChannel.Dispatcher.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NotificationController : ControllerBase
+    {
+
+        public async Task<IActionResult> SendNotification()
+        {
+
+        }
+        
+    }
+}
