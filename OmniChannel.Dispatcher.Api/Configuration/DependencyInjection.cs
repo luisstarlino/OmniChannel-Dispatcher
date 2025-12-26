7c70c988c0c0224de338f57c1e2af4f68ce38e6b
Luis Starlino <luis.guilherme009@gmail.com>
2025-12-26 11:33:35 -0300

    feat(patter): add strategy pattern to solve the main problem

---
+++ OmniChannel.Dispatcher.Api/Configuration/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using OmniChannel.Dispatcher.Core.Interfaces;
+using OmniChannel.Dispatcher.Core.Services;
+using OmniChannel.Dispatcher.Core.Strategies;
+
+namespace OmniChannel.Dispatcher.Api.Configuration
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddOmniChannelServices(this IServiceCollection services)
+        {
+            // --- Patter
+            services.AddScoped<INotificationStrategy, MailStrategy>();
+            services.AddScoped<INotificationStrategy, PushStrategy>();
+            services.AddScoped<INotificationStrategy, SmsStrategy>();
+
+            // --- Service
+            services.AddScoped<INotificationService, NotificationService>();
+
+            return services;
+        }
+    }
+}
