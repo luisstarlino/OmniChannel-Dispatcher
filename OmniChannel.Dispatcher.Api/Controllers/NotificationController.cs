d8a9c0c352144264d609faf4aa1285a13533d102
Luis Starlino <luis.guilherme009@gmail.com>
2025-12-26 15:09:03 -0300

    feat(strategy): finish the core app

--- OmniChannel.Dispatcher.Api/Controllers/NotificationController.cs
+++ OmniChannel.Dispatcher.Api/Controllers/NotificationController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OmniChannel.Dispatcher.Api.DTOs;
+using OmniChannel.Dispatcher.Core.Interfaces;
 
 namespace OmniChannel.Dispatcher.Api.Controllers
 {
@@ -7,10 +8,29 @@ namespace OmniChannel.Dispatcher.Api.Controllers
     [Route("[controller]")]
     public class NotificationController : ControllerBase
     {
+        private readonly INotificationService _notificationService;
+
+        public NotificationController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
         [HttpPost]
         public async Task<IActionResult> SendNotification([FromBody] SendDTO model)
         {
-            return Ok(model);
+            try
+            {
+                await _notificationService.DispatchMessage(model.Channel, model.Message);
+                return Ok(model);
+
+            } catch (Exception ex)
+            {
+                return Problem(
+                    detail: ex.Message,
+                    title: "Internal server error"
+                );
+            }
+
         }
         
     }
